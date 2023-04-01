73016b507225ae394624503a4ab3ea39f5c0ff1a
Debaditya Sen <debadityasen17@gmail.com>
2023-04-02 00:11:42 +0530

    Merging Changes Pt. 2

--- API/Program.cs
+++ API/Program.cs
@@ -5,8 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
